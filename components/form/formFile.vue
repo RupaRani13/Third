72c3b7bf156fb2d6a2af437b5596bff8facf239d
@asmu66614 <asmu66614@gmail.com>
2022-12-16 17:12:54 +0530

    submit show

--- components/form/formFile.vue
+++ components/form/formFile.vue
@@ -1,20 +1,23 @@
 <template>
     <div>
-        <!-- <Imageupload></Imageupload> -->
-        <v-form v-model="valid" @submit.prevent="onSubmit()" ref="form">
+        <v-form v-model="valid" @submit.prevent="onSubmit()" ref="form" >
             <v-responsive class="mx-auto" max-width="1000" pb-4>
-                <div v-for="item in formFields" :key="item.id">
-                    <UiFormDesign01 :label="item.title" :type="item.type" :required='item.required'
-                        :options="item.options" :fileType="item.fileType" :fileSize="parseInt('10000')"
-                        v-model="userData[item.controlName]" :controlName="item.controlName">
-                    </UiFormDesign01>
+                <div id="formFile">
+                    <div v-for="item in formFields" :key="item.id">
+                        <UiFormDesign01 :label="item.title" :type="item.type" :required='item.required'
+                            :options="item.options" :fileType="item.fileType" :fileSize="parseInt('10000')"
+                            v-model="userData[item.controlName]" :controlName="item.controlName">
+                        </UiFormDesign01>
+                    </div>
+                    <v-btn class="btn" type="submit">submit</v-btn>
                 </div>
-                <v-btn class="btn" type="submit">submit</v-btn>
+                <div id="submitmessage">
+                 <p></p>
+                </div>
             </v-responsive>
         </v-form>
     </div>
 </template>
-
 <script>
 
 export default {
@@ -25,18 +28,13 @@ export default {
         const formFields = ref(null);
         formFields.value = formdata.fields;
         console.log(formFields.value, ' formFields.value')
-        // formFields.value.forEach(item => {
-        //     userData[item.controlName] = "";
-        // });
         console.log(userData, formFields);
         return {
             formFields, userData
-
         };
     },
     data() {
         return {
-            // newUserData: {},
             fileModel: null,
             valid: true,
             terms: false,
@@ -44,23 +42,24 @@ export default {
         }
     },
     methods: {
-        // myFunction(a, b) {
-        //     this.userData[b] = a;
-        //     console.log(this.userData);
-        // },
         async submitUserData(userData) {
             const apiUrl = `https://demo02.institute.org.in/api/form/formresponse`
             const data = {
                 form: '6388487e8e13e93f22cbe9c4',
                 response: userData,
-               
             }
-            // alert(1);
             await $fetch(apiUrl, { method: 'POST', body: data }).then(res => {
                 console.log(res)
-                debugger
-                newUserData = this.$refs.form.reset()
-                debugger
+                var hide = this.$el.querySelector("#formFile");
+                hide.style.display = 'none';
+                var sunmitshow = this.$el.querySelector("#submitmessage");
+                sunmitshow.style.display = 'block';   
+                sunmitshow.innerText = 'Your form Successfully submitted';
+                // for (let key in this.userData) {
+                //     this.userData[key] = "";    
+                // }
+                // this.refs.form.resetValidation()
+                // this.userData = {}
             }).catch(e => console.log(e))
         },
         checkValidity() {
@@ -104,10 +103,9 @@ export default {
                     }
                 }
                 this.submitUserData(newUserData);
+
                 console.log(newUserData, 'userdata')
-                // this.$refs.form.reset()
-                debugger
-                this.$refs.form.resetValidation();
+
             } else {
                 return
             }
@@ -116,6 +114,8 @@ export default {
         handleFileUpload(e) {
             console.log(e.target)
         },
+
+
     },
 }
 
@@ -183,4 +183,11 @@ export default {
 #fromDesign01 .titlesec .checkboxmsg {
     height: 75px;
 }
+/* #submitmessage{
+    font-size: 24px;
+    font-weight: 500;
+    border: 2px solid #03a84e;
+    border-radius: 4px;
+    padding: 20px;
+} */
 </style>
--- components/imageupload.vue
+++ components/imageupload.vue
@@ -1,10 +1,13 @@
 <template>
-    <v-form>
+    <v-form ref="form">
         <v-card height="100vh">
             <v-file-input v-model="myFile" label="File input" @update:modelValue="onFileSelected"></v-file-input>
             <button @click="onUpload">Submit</button>
+            <v-file-input  label="label"
+                    v-model="tempValue" clearable  rows="4" show-size="1024" accept="image/png, image/jpeg, image/bmp" row-height="30" @change="onFileSelected(tempValue)" :rules="tempValue? [rules.fileSize]: [rules.fileRequired]">
+                </v-file-input>
+                <v-btn @click="newMethod()" >new btn</v-btn>
         </v-card>
-
     </v-form>
 
 
@@ -16,11 +19,23 @@
 export default {
     data() {
         return {
+            tempValue:[],
             selectedFile: null,
             myFile : '',
+            fileSize : 10000,
+            rules: {
+                required: value => !!value || 'Required.',
+                fileSize: value => (value && value[0] && value[0].size < fileSize) || `File size should be less than ${ fileSize / 1048576} MB`,
+                fileRequired: value => (value && value.length) || 'File is required',
+            },
         }
     },
     methods: {
+        newMethod(){
+            alert("hello")
+            debugger
+            this.$refs.form.resetValidation()
+        },
         onFileSelected() {
             console.log(this.myFile)
             this.selectedFile = this.myFile[0];
