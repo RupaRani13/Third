081a7f0ea94ffeb0121ae4aa730438818a98701a
Akshanshu Pal <akshanshupal@gmail.com>
2022-12-17 07:02:10 +0000

    Update components/ui/form/Design_02.vue

--- components/ui/form/Design_02.vue
+++ components/ui/form/Design_02.vue
@@ -9,6 +9,7 @@
             </v-text-field>
         </template>
         <template v-if="type == 'mobile'">
+            <v-text-field validate-on="input" v-if="required" :label="`${label}*`"
                 v-model="modelValue"   @input="$emit('update:modelValue', $event.target.value)" :rules="modelValue ? [rules.mobileNumber] : [rules.required]" >
             </v-text-field>
             <v-text-field validate-on="input" v-else :label="label"
@@ -16,6 +17,7 @@
             </v-text-field>
         </template>
         <template v-if="type == 'email'">
+            <v-text-field v-if="required" validate-on="input" :label="`${label}*`"
                 v-model="modelValue"   @input="$emit('update:modelValue', $event.target.value)" :rules="modelValue ? [rules.email] : [rules.required]" >
             </v-text-field>
             <v-text-field  v-else validate-on="input" :label="label"
@@ -23,6 +25,20 @@
             </v-text-field>
         </template>
         <template v-if="type == 'number'">
+            <v-text-field v-if="required" validate-on="input" :label="`${label}*`"
+                v-model="modelValue"   @input="$emit('update:modelValue', $event.target.value)" :rules="modelValue ? [rules.number] : [rules.required]" >
+            </v-text-field>
+            <v-text-field v-else validate-on="input" :label="label"
+                v-model="modelValue"   @input="$emit('update:modelValue', $event.target.value)" :rules="modelValue ? [rules.number] : ''" >
+            </v-text-field>
+        </template>
+        <template v-if="type == 'textarea'">
+            <v-text-field  v-if="required" validate-on="input" :label="`${label}*`"
+                v-model="modelValue" rows="4" row-height="30" @input="$emit('update:modelValue', $event.target.value)" :rules="[rules.required]" >
+            </v-text-field>
+            <v-text-field v-else validate-on="input"  :label="label"
+                v-model="modelValue" rows="4" row-height="30" @input="$emit('update:modelValue', $event.target.value)"  rules="" >
+            </v-text-field>
         </template>
         <template v-if="type == 'file'">
                 <v-file-input v-if="required" :label="`${label}*`"
@@ -38,6 +54,7 @@
                     <v-radio v-for="item in options" :key="item" :label="item" :value="item" color="success">
                     </v-radio>
                 </v-radio-group>
+            </div>
         </template>
         <template v-if="type == 'dropdown'">
             <client-only>
@@ -46,6 +63,29 @@
             </v-select>
             </client-only>
         </template>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    setup() {
+
+    },
+    props: {
+        label: {
+            default: ''
+        },
+        type: {
+            type: String,
+            default: 'text'
+        },
+        required: {
+            type: Boolean,
+            default: false,
+        },
+        options: {
             type: Array
         },
         fileType: {
@@ -53,6 +93,10 @@
         },
         fileSize: {
             default : 1*1048576,
+            type: Number
+        },
+        modelValue: {
+            type: String
         },
         controlName : {
             type : String
@@ -60,6 +104,17 @@
     },
     data() {
         return {
+            rules: {
+                required: value => !!value || 'Required.',
+                requiredSelect: (value) => value.length > 0 || "Value is required!!",
+                number: value => Number.isInteger(Number(value)) || "The value must be an integer number",
+                mobileNumber: value => (value > 1000000000 && value < 9999999999) || 'Enter a valid 10 digit Mobile Number',
+                email: value => (value != '' && /.+@.+/.test(value)) || 'E-mail must be valid',
+                fileSize : value => (value&& value[0] && value[0].size < this.$props.fileSize) || `File size should be less than ${this.$props.fileSize/1048576} MB`,
+                fileRequired : value => (value&& value.length) || 'File is required',
+            },     
+        }
+    },
     methods:{
         newFun(event){
             this.$refs.fileForm.validate();
@@ -71,7 +126,13 @@
             // this.selectedFile = event.target.files[0]
             // console.log(this.selectedFile, 'file');
             // if (this.selectedFile.size < 1024) {
-
+            //     return
+            // } else {
+            //      alert('file size should be less than 1MB')
+            //      event.target.value = '';
+            //      this.selectedFile.size.reset()
+            //      this.$refs.event.target.value= null
+            // this.$refs.selectedFile = null
             // }
         },
         onUpload() {
@@ -91,3 +152,5 @@
     },
 
 }
+
+</script>
