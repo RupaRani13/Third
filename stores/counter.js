9c53a3ac766c002407546b7df94a263c3451a10e
akshanshupal1 <akshanshupal@gmail.com>
2022-12-27 20:46:55 +0530

    asp code

--- stores/counter.js
+++ stores/counter.js
@@ -1,27 +1,4 @@
 import { defineStore } from 'pinia'
-<<<<<<< HEAD
-
-// export const useCounterStore = defineStore('counter', {
-//   state: () => {
-//     return { count: 0 }
-//   },
-
-//   actions: {
-//     increment() {
-//       this.count++
-//     },
-//   },
-// })
-
-export const useCounterStore = defineStore('counter', () => {
-    const count = ref(0)
-    function increment() {
-      count.value++
-    }
-  
-    return { count, increment }
-  })
-=======
 export const useCounterStore = defineStore('counter', {
   state: () => {
     return { 
@@ -46,18 +23,3 @@ export const useCounterStore = defineStore('counter', {
   },
 
 })
-
-
-
-// export const useCounterStore = defineStore('counter', () => {
-//     const count = ref(0)
-//     function increment() {
-//       count.value++
-//     }
-  
-//     return { count, increment }
-//   })
-
-
-   
->>>>>>> a95fe66dc384a94f362f635aa4261b774223e535
